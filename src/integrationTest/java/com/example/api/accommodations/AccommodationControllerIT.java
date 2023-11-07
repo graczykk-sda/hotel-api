455a6e7d293d3cf9821cbe1a1853f1d676154cad
graczykk <krzysztof.graczyk@tutanota.com>
2023-11-07 14:17:26 +0100

    corrections

--- src/integrationTest/java/com/example/api/accommodations/AccommodationControllerIT.java
+++ src/integrationTest/java/com/example/api/accommodations/AccommodationControllerIT.java
@@ -2,7 +2,6 @@ package com.example.api.accommodations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
